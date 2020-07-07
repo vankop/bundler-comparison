cca7193135b2d351cd0813d93329ce70a9c39c96
Tobias Koppers <tobias.koppers@googlemail.com>
2020-07-07 15:49:18 +0300

    disable iife wrapping in webpack
    (cherry picked from commit a6c4783092b586c584e5f1bb82657a5cf16e9c1e)

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,24 @@ module.exports = {
 	output: {
 		path: require('path').resolve('results'),
 		filename: 'webpack.js',
-		libraryTarget: 'commonjs'
+		iife: false
 	},
 	module: {
 		rules: [
 			{
 				type: "javascript/esm"
 			}
+		]
+	},
+	optimization: {
+		minimizer: [
+			new Terser({
+				terserOptions: {
+					compress: {
+						passes: 2
+					}
+				}
+			})
 		]
 	},
 	devtool: 'source-map',
