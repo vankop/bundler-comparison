f97c68815a554f3ea5c8b4d2514dacb020c2b8c8
Tobias Koppers <tobias.koppers@googlemail.com>
2020-07-07 15:22:12 +0200

    make build index deterministic with non-case-senstive file systems

--- build-index.js
+++ build-index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const index = fs.readdirSync('lodash')
 	.filter(file => !file.startsWith('.') && file.endsWith('.js'))
+	.sort()
 	.map(file => `export { default as ${file.slice(0, -3)} } from './lodash/${file}';`).join('\n');
 
 fs.writeFileSync('index.js', index);
